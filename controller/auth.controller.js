db50b95964b6f393eedcbaf5f546c6333c96bcae
Balveer <balveermahor88599@gmail.com>
2025-12-20 01:28:03 +0530

    create signuo controller for sighup page in frontend

---
+++ controller/auth.controller.js
@@ -0,0 +1,30 @@
+import User from "../models/user.models";
+import bcrypt from "bcryptjs";
+const signUp = async(req, res) => {
+  try {
+    const { Fullname, email, password, mobile, role } = req.body;
+    const UserData = await User.findOne({ email });
+    if (UserData) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password must be at least 6 characters long" });
+    }
+    if (mobile.length < 10) {
+      return res.status(400).json({ message: "Mobile number must be at least 10 digits" });
+    }
+    const hassedPassword = await bcrypt.hash(password, 10);
+    user = await User.create({
+      Fullname,
+      email,
+      password: hassedPassword,
+      mobile,
+      role,
+    });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error during sign up:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
