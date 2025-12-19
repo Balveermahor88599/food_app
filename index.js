cab0534ada8e23ca15c118037b06092e75c08cd6
Balveer <balveermahor88599@gmail.com>
2025-12-20 02:19:58 +0530

    create defaukt middleware in index,js for data converted in json form

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+import router from './routes/auth.routes.js';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());    
+app.use(cookieParser());
+app.use('/api/auth',router) ;
 
 app.listen(PORT, () => {
     connectDB()
